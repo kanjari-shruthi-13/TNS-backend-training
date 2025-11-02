ffb802b31ccd2ded4154fb35ab4fa6314649ea56
kanjari shruthi <kanjarishruthi@gmail.com>
2025-11-02 18:00:15 +0530

    Initial commit

---
+++ Car.java
@@ -0,0 +1,12 @@
+//Demo for Class Car 
+//Structure of the Class
+public class Car { 
+	//Variables 
+	int speed; 
+	String model;
+	String type; 
+	//Method 
+	void Start() {
+		System.out.println("Car model: " + model + ", Type: " + type + ", Speed: " + speed + " kmph");
+}
+}
---
+++ CarMain.java
@@ -0,0 +1,16 @@
+public class CarMain {
+
+	public static void main(String[] args) {
+		//creating an object 
+		//objectname.classname=new constructor();
+		Car c=new Car(); 
+		c.speed =120;
+		c.model ="Honda"; 
+		c.type ="petrol";
+		// Calling the method to display output
+        c.Start();
+
+	}
+
+}
+    
