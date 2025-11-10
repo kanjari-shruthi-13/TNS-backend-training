7a22de97685ac04d42c7ed36908302fbbdf19e73
Shruthi Kanjari <kanjarishruthi@gmail.com>
2025-11-10 21:45:22 +0530

    Update CarMain.java

--- CarMain.java
+++ CarMain.java
@@ -1,17 +1,14 @@
+// CarMain.java
+/**
+ * Demonstrates the use of the Car class.
+ */
 public class CarMain {
 
-	public static void main(String[] args) {
-		//creating an object 
-		//objectname.classname=new constructor();
-		Car c=new Car(); 
-		c.speed =120;
-		c.model ="Honda"; 
-		c.type ="petrol";
-		// Calling the method to display output
-        c.Start();
+    public static void main(String[] args) {
+        // Creating a Car object using the constructor
+        Car car = new Car("Honda", "Petrol", 120);
 
-	}
-
+        // Displaying car details
+        car.start();
+    }
 }
-    
-
